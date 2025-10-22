b1c270970f594af6e1abe5525658fb316f02ec0a
joule <julietamari@gmail.com>
2025-10-22 14:47:18 -0300

    Add database backup command to db_config.sh and create a new GitHub Actions workflow for manual production migrations. The workflow includes SSH setup and execution of the db_config.sh script for applying migrations.

--- db_config.sh
+++ db_config.sh
@@ -1,2 +1,3 @@
+docker exec lb-premium-db pg_dump -U postgres lb_premium > /tmp/backup-$(date +%Y%m%d-%H%M%S).sql 
 docker exec  lb-premium-backend npm run db:migrate
 docker exec  lb-premium-backend npm run db:seed
